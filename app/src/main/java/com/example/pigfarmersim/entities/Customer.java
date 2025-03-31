69bffbfcb4bcdf3d9e9e47cf43dcc538c639b030
Daniel <ggsan.com@gmail.com>
2025-03-31 18:14:09 +0800

    First commit

---
+++ app/src/main/java/com/example/pigfarmersim/entities/Customer.java
@@ -0,0 +1,29 @@
+package com.example.pigfarmersim.entities;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.example.pigfarmersim.MainActivity;
+import com.example.pigfarmersim.R;
+import com.example.pigfarmersim.helpers.GameConstants;
+import com.example.pigfarmersim.helpers.interfaces.BitmapMethods;
+
+public enum Customer implements BitmapMethods{
+
+    NEW_CUSTOMER(R.drawable.customer_new_spritesheet);
+
+    private Bitmap spriteSheet;
+    private Bitmap[][] sprites = new Bitmap[4][4];
+
+    Customer(int resID) {
+        options.inScaled = false;
+        spriteSheet = BitmapFactory.decodeResource(MainActivity.getGameContext().getResources(), resID, options);
+        for (int j = 0; j < sprites.length; j++)
+            for (int i = 0; i < sprites[j].length; i++)
+                sprites[j][i] = getScaledBitmap(Bitmap.createBitmap(spriteSheet, GameConstants.Sprite.DEFAULT_SIZE * i, GameConstants.Sprite.DEFAULT_SIZE * j, GameConstants.Sprite.DEFAULT_SIZE, GameConstants.Sprite.DEFAULT_SIZE));
+    }
+
+    public Bitmap getSprite(int yPos, int xPos) {
+        return sprites[yPos][xPos];
+    }
+}
