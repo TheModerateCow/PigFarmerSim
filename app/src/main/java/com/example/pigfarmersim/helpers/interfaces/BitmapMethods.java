23074f89016f957a6d964ef254f12a306c2567d7
Bryan <93906780+TheModerateCow@users.noreply.github.com>
2025-04-20 21:15:17 +0800

    Refactor: Modify BitmapMethods for character scaling
    - Changed `getScaledCharacterBitmap` method to be package-private.

--- app/src/main/java/com/example/pigfarmersim/helpers/interfaces/BitmapMethods.java
+++ app/src/main/java/com/example/pigfarmersim/helpers/interfaces/BitmapMethods.java
@@ -17,7 +17,7 @@ public interface BitmapMethods {
         return Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, false);
     }
 
-    public static Bitmap getScaledCharacterBitmap(Bitmap bitmap) {
+    static Bitmap getScaledCharacterBitmap(Bitmap bitmap) {
         // scaling logic here
         return Bitmap.createScaledBitmap(bitmap, bitmap.getWidth() * 8, bitmap.getHeight() * 8, false);
     }
