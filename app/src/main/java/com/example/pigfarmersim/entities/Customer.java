10432d090f6c05640eccae8af1a97d861d1178c3
Bryan <93906780+TheModerateCow@users.noreply.github.com>
2025-04-20 21:18:10 +0800

    Refactor: Replace GameCharacters class with CharacterSprites enum
    - Removed the `GameCharacters` class.
    - Introduced the `CharacterSprites` enum to manage character sprite sheets.
    - The enum now handles loading and scaling sprites, previously managed by the deleted class.

--- app/src/main/java/com/example/pigfarmersim/entities/Customer.java
+++ app/src/main/java/com/example/pigfarmersim/entities/Customer.java
@@ -12,12 +12,11 @@ public enum Customer implements BitmapMethods{
 
     CUSTOMER(R.drawable.customer_new_spritesheet);
 
-    private Bitmap spriteSheet;
-    private Bitmap[][] sprites = new Bitmap[4][4];
+    private final Bitmap[][] sprites = new Bitmap[4][4];
 
     Customer(int resID) {
         options.inScaled = false;
-        spriteSheet = BitmapFactory.decodeResource(MainActivity.getGameContext().getResources(), resID, options);
+        Bitmap spriteSheet = BitmapFactory.decodeResource(MainActivity.getGameContext().getResources(), resID, options);
         for (int j = 0; j < sprites.length; j++)
             for (int i = 0; i < sprites[j].length; i++)
                 sprites[j][i] = getScaledBitmap(Bitmap.createBitmap(spriteSheet, GameConstants.Sprite.DEFAULT_SIZE * i, GameConstants.Sprite.DEFAULT_SIZE * j, GameConstants.Sprite.DEFAULT_SIZE, GameConstants.Sprite.DEFAULT_SIZE));
